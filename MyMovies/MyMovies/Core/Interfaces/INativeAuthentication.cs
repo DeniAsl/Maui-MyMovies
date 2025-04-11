c51d4cd5b2dc5543429e9e127d578f0110e0527a
DeniAsl <deni.aslakhanov@student.howest.be>
2025-04-12 01:10:18 +0200

    Add in progress Authentication

---
+++ MyMovies/MyMovies/Core/Interfaces/INativeAuthentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMovies.Core.Interfaces
+{
+    interface INativeAuthentication
+    {
+        // <summary>
+        /// Checks whether the current platform supports native authentication
+        /// </summary>
+        bool IsSupported();
+        /// <summary>
+        /// Prompt the user to authenticate using the native authentication system
+        /// </summary>
+        Task<AuthenticationResult> PromptLoginAsync(string prompt);
+    }
+}
---
+++ MyMovies/MyMovies/Platforms/Android/Services/NativeAuthentication.Android.cs
@@ -0,0 +1,22 @@
+﻿using MyMovies.Core.Interfaces;
+using MyMovies.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMovies.Platforms.Services
+{
+    public class NativeAuthentication : INativeAuthentication
+    {
+        public bool IsSupported()
+        {
+            return false; // this stub is not ready, so feature is not supported
+        }
+        public async Task<AuthenticationResult> PromptLoginAsync(string prompt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
